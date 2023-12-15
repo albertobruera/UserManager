37533d10f55559f00d5d584e4ccaf2d89ce33e33
alberto.bruera <alberto.bruera72@gmail.com>
2023-12-15 23:22:00 +0100

    create onInit for hook 	modified:   src/app/users/users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { UserService } from '../user.service';
 
@@ -10,11 +10,16 @@ import { UserService } from '../user.service';
   ]
 })
 
-export class UsersComponent {
+export class UsersComponent implements OnInit {
   title = 'Users'
   public users:any[] = [];
 
   constructor(private service: UserService) {
+    }
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
     this.users = UserService.getUsers();
+
   }
 }

207a8b48a1415b2a751c13cefb649d4d5276c6e6
alberto.bruera <alberto.bruera72@gmail.com>
2023-12-15 16:37:07 +0100

    modified:   src/app/users/users.component.html 	modified:   src/app/users/users.component.ts

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-users',
@@ -9,5 +10,35 @@ import { Component } from '@angular/core';
 })
 
 export class UsersComponent {
+  title = 'Users'
+  users = [
+    {
+      name: 'Alberto',
+      lastname: 'Bruera',
+      fiscalcode: 'BRRLRT72R30E758J',
+      email: 'alberto.bruera72@gmail.com',
+      phone: '+393381195546',
+      province: 'CUNEO',
+      Age: '51'
+    },
+    {
+      name: 'Pippo',
+      lastname: 'Bruera',
+      fiscalcode: 'PPPLRT72R30E758J',
+      email: 'pippo.bruera72@gmail.com',
+      phone: '+393381195546',
+      province: 'CUNEO',
+      Age: '51'
+    },
+    {
+      name: 'Pluto',
+      lastname: 'Bruera',
+      fiscalcode: 'PPPLRT72R30E758J',
+      email: 'pluto.bruera72@gmail.com',
+      phone: '+393381195546',
+      province: 'CUNEO',
+      Age: '51'
+    }
+  ]
 
 }
